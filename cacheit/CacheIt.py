9399c778e1324ba288024786b942b996290e63a7
Dmitrii Pukhov <pukhovdn@yandex.ru>
2019-08-19 23:49:36 +0900

    Add test for hash numpy method. Make it static (#8)
    Relates #1

--- cacheit/CacheIt.py
+++ cacheit/CacheIt.py
@@ -29,7 +29,8 @@ class CacheIt:
         sha224 = hashlib.sha224
         return sha224(input_string.encode()).hexdigest()
 
-    def _hash_ndarray(self, input_array):
+    @staticmethod
+    def _hash_ndarray(input_array):
         if not isinstance(input_array, np.ndarray):
             raise CacheItValueError(
                 "numpy.ndarray instance was expected but got {}".format(
@@ -37,7 +38,7 @@ class CacheIt:
                 )
             )
         string = input_array.data.hex()
-        return self._hash_string(string)
+        return CacheIt._hash_string(string)
 
     def _hash_object(self, entity):
         """Converting to string non-supported by JSON objects.
