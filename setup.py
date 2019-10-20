3755151612a9c833621dea143b0cf967aac65499
Dmitrii Pukhov <pukhovdn@yandex.ru>
2019-10-20 11:14:11 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "0.0.1"
+version = "0.0.3"
 
 install_requires = [
     'pymemcache>=2.2.2',
@@ -9,15 +9,28 @@ install_requires = [
     'pandas>=0.25.0',
 ]
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name='keepthis',
     version=version,
-    packages=['keepthis'],
-    install_requires=install_requires,
-    url='github.com/puhoshville',
-    license='MIT',
     author='Dmitrii Pukhov',
     author_email='pukhovdn@yandex.ru',
     description='KeepThis - data scientist helper, tool for caching artifacts which allow easily to share it with '
-                'your teammates.'
+                'your teammates.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/puhoshville/keep-this",
+    packages=['keepthis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    license='MIT',
 )
+
